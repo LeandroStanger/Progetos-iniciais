f08d80905c69fe5f8d1e53e771235eba4b127b06
leandrostanger <leandrostanger@hotmail.com>
2025-04-27 10:40:24 -0300

    Meu primeiro commit 01-Ola-Mundo.js

---
+++ Aula1/01-Ola-Mundo.js
@@ -0,0 +1,9 @@
+let pessoa = "Joao"; //Aceita mudansas
+const idadePessoa = 29 +3; //Nao aceita mudansas
+pessoa = "Lucas";
+//idadePessoa = 25
+console.log ("Ola Mundo");
+console.log (pessoa);
+console.log ("O seu nome e: ", pessoa,"e sua idade e: ", idadePessoa);
+
+console.log ("SINHO", pessoa, "tem", idadePessoa, " comparesa ao SUS?")
