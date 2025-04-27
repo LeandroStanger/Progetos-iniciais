88687cb30309be0a0c59c0383d0fb7aac0426634
leandrostanger <leandrostanger@hotmail.com>
2025-04-27 14:16:19 -0300

    Meu terceiro commit CalcularVelocidade.js

---
+++ Aula1/CalcularVelocidade.js
@@ -0,0 +1,9 @@
+//Crie uma função chamada calcularVelocidade que recebe a distância e o tempo de uma viagem como parâmetros e retorna a velocidade média da viagem. Utilize a função para imprimir a mensagem: "A velocidade média da viagem é: ", seguida do resultado da divisão da distância pelo tempo.
+function calcularVelocidade(velocidade01, velocidade02){
+    return (velocidade01 + velocidade02) / mediaDeVelocidade;
+}
+let velocidade01 = 40;
+let velocidade02 = 80;
+let mediaDeVelocidade = 2;
+
+console.log ("A velocidade média da viagem é: ", calcularVelocidade(velocidade01, velocidade02, mediaDeVelocidade), "por horas!");
