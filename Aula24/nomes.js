b14eaa613e9e44ea48be71a61f7b2bbc8b607dfb
Leandro Stanger <leandrostanger@hotmail.com>
2025-08-30 09:38:10 -0300

    Add nomes.js

---
+++ Aula24/nomes.js
@@ -0,0 +1,5 @@
+let nomes = ["Ana", "Carlos", "Julia", "Pedro", "Igor", "Rafa"];
+
+let temJ = nomes.some(nomes => nomes[0] === "J");
+
+console.log(`Existe alguém com nome começando por J: ${temJ}`)
