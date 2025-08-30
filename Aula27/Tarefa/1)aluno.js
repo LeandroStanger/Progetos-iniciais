e49149bb765f8f395924fb61f1a2677457241885
Leandro Stanger <leandrostanger@hotmail.com>
2025-08-30 09:51:16 -0300

    ADD 1)aluno.js

---
+++ Aula27/Tarefa/1)aluno.js
@@ -0,0 +1,9 @@
+function aluno() {
+    const notas = [9, 8, 9, 7, 9];
+
+    const media = notas.reduce((acumulador, valorAtual) => acumulador + valorAtual, 0) / notas.length;
+
+    console.log(media);
+}
+
+aluno();
