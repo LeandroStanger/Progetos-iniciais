e4ba04a5befeaab30f4915ade9116db38336584c
Leandro Stanger <leandrostanger@hotmail.com>
2025-09-07 09:14:55 -0300

    Add Aula30

---
+++ Aula30/verificarUsuarioEmail-Part2.js
@@ -0,0 +1,26 @@
+function verificarUsuario(nome, email) {
+  let cadastros = [
+    { nome: "Elias", email: "elias@email.com", idade: 38 },
+    { nome: "Maria", email: "maria@email.com", idade: 30 },
+    { nome: "João", email: "joao@email.com", idade: 25 },
+    { nome: "Ana", email: "ana@email.com", idade: 22 }
+  ];
+  const part = cadastros.map(lista => lista.nome.toUpperCase());
+  const part2 = part.includes(nome.toUpperCase());
+
+  const part3 = cadastros.map(lista => lista.email.toUpperCase());
+  const part4 = part3.includes(email.toUpperCase());
+  //console.log(part4)
+
+  if (part2 && part4) {
+    return(`Você está na lista! Temos atualmente ${cadastros.length} cadastros: \n${part} \n${part3}`);
+  } else if (!part2 && nome && !part4 && email) {
+    return(`Seu nome não está na lista. \nNomes cadastrados: ${part} \nEmail cadastrados: ${part3}`);
+  } else {
+    return(`Por favor, preencha todos os campos corretamente.`);
+  }
+
+}
+console.log(verificarUsuario("Elias", "elias@email.com", 38));
+console.log(verificarUsuario("Carlos", "carlos@email.com", 25));
+console.log(verificarUsuario("", "teste@email.com", 20));
